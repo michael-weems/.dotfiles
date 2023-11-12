9858e119f12966e304235c74477fd211a0e0bd49
Michael Weems <mweems5@gmail.com>
2023-11-12 13:12:32 -0600

    WIP: get the config working again - telescope is broken

--- lua/michaelweems/remap.lua
+++ lua/michaelweems/remap.lua
@@ -1,50 +1,66 @@
-
+-- fat cursor in insert mode
 vim.g.mapleader = " "
+
+-- project explorer view
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+-- Move highlighted lines up and down with shift + j / shift + k
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+-- Append below line to current line while keeping cursor in place
 vim.keymap.set("n", "J", "mzJ`z")
+
+-- Jump halfway up and down document while keeping cursor centered
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- ? not sure
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
-vim.keymap.set("n", "<leader>vwm", function()
-    require("vim-with-me").StartVimWithMe()
-end)
-vim.keymap.set("n", "<leader>svwm", function()
-    require("vim-with-me").StopVimWithMe()
-end)
-
--- greatest remap ever
+-- ? forgot what this does
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
--- next greatest remap ever : asbjornHaland
+-- 'y' --> yank to vim clipboard
+-- '<leader>y' --> yank to system clipboard
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
+-- '<leader>d' actions --> send deleted to void instead of buffer
 vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
 
--- This is going to get me cancelled
+-- ? not sure what this does....
 vim.keymap.set("i", "<C-c>", "<Esc>")
 
+-- ? apparently I should never be in 'Q' mode
 vim.keymap.set("n", "Q", "<nop>")
+
+-- Jump between projects! this is cool
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+
+-- ? possibly format the document?
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
+-- ? what do these do?
 vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
+-- ? what does this do?
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+-- set a file's permissions to allow it to be executable
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
-vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.dotfiles/nvim/.config/nvim/lua/theprimeagen/packer.lua<CR>");
+-- ? not sure what this does
+vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.dotfiles/nvim/.config/nvim/lua/michaelweems/packer.lua<CR>");
+
+-- ? what?
 vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
 
+-- shortcut to sourcing a file or "shouting out" a file
 vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
 end)
